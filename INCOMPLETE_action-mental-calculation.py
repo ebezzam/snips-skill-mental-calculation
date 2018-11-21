22197d2f4ed63338350172ea2805bb55ab4eb14e
Eric B <eric.bezzam@epfl.ch>
2018-11-21 16:41:25 +0100

    Adding incomplete code for workshop purposes.

--- INCOMPLETE_action-mental-calculation.py
+++ INCOMPLETE_action-mental-calculation.py
@@ -6,6 +6,7 @@ MQTT_IP_ADDR = "localhost"
 MQTT_PORT = 1883
 MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
 
+# replace `bezzam` with your username if using your own intents!!
 INTENT_START = "bezzam:start_mental_calculations"
 INTENT_ANSWER = "bezzam:give_mental_calculation"
 INTENT_STOP = "bezzam:stop_lesson"
@@ -18,11 +19,28 @@ INTENT_FILTER_GET_ANSWER = [
 ]
 
 operations = ["add", "sub", "mul", "div"]
-
 SessionsStates = {}
 
 
+def main():
+
+    # subscribe to the intents of desire and point to the corresponding callback function
+    with Hermes(MQTT_ADDR) as h:
+        h.subscribe_intent(INTENT_START, user_request_quiz) \
+            .subscribe_intent(INTENT_STOP, user_quits) \
+            .subscribe_intent(INTENT_DOES_NOT_KNOW, user_does_not_know) \
+            .subscribe_intent(INTENT_ANSWER, user_gives_answer) \
+            .start()
+
+
 def create_question(oper=None):
+    """
+    Randomly create a question with two terms.
+
+    :param oper: Which operation ("add", "sub", "mul", "div") you would like to focus on (TODO). Default will randomly
+    pick one of four.
+    :return:
+    """
 
     if oper is None or oper not in operations:
         oper = random.choice(operations)
@@ -52,6 +70,13 @@ def create_question(oper=None):
 
 
 def continue_lesson(response, session_id):
+    """
+    Update state, and continue/end lesson depending on number of questions left.
+
+    :param response: Beginning of response to the user (whether the answer was correct/wrong).
+    :param session_id: Session ID to keep track on remaining questions.
+    :return:
+    """
     SessionsStates[session_id]["step"] += 1
 
     if SessionsStates[session_id]["step"] == SessionsStates[session_id]["n_questions"]:
@@ -80,15 +105,9 @@ def continue_lesson(response, session_id):
 def user_request_quiz(hermes, intent_message):
     session_id = intent_message.session_id
 
-    # parse input message, NOTE extra space to append question
+    # parse intent_message and generate response (don't forget extra space for question!)
     n_questions = int(intent_message.slots.n_questions.first().value)
-    if n_questions > 1:
-        response = "Starting a lesson with {} questions. ".format(n_questions)
-    elif n_questions == 1:
-        response = "Starting a lesson with one question. "
-    else:
-        response = "I can only give a positive number of questions."
-        hermes.publish_end_session(session_id, response)
+    response
 
     # create first question
     question, answer = create_question()
@@ -103,42 +122,33 @@ def user_request_quiz(hermes, intent_message):
     }
     SessionsStates[session_id] = session_state
 
+    # continue session since we need to answer the questions!
     hermes.publish_continue_session(session_id, response + question, INTENT_FILTER_GET_ANSWER)
 
 
 def user_gives_answer(hermes, intent_message):
-    session_id = intent_message.session_id
 
-    # parse input message
-    answer = intent_message.slots.answer.first().value
+    # parse intent_message
+    answer
 
-    # check user answer, NOTE the extra space at the end since we will add more to the response!
-    if answer == SessionsStates[session_id]["ans"]:
-        response = "Correct! "
-        SessionsStates[session_id]["good"] += 1
-    else:
-        response = "Incorrect. The answer is {}. ".format(SessionsStates[session_id]["ans"])
-        SessionsStates[session_id]["bad"] += 1
+    # check user answer and give feedback (don't forget extra space!)
+    response
 
-    # create new question or terminate if reached desired number of questions
+    # create new question or terminate if reached desired number of questions (`cont=False`)
     response, cont = continue_lesson(response, session_id)
-    if cont:
-        hermes.publish_continue_session(intent_message.session_id, response, INTENT_FILTER_GET_ANSWER)
-    else:
-        hermes.publish_end_session(session_id, response)
+
+    # different hermes publish based on value of `cont`
 
 
 def user_does_not_know(hermes, intent_message):
-    session_id = intent_message.session_id
 
-    response = "That's quite alright! The answer is {}. ".format(SessionsStates[session_id]["ans"])
+    # some reassuring message
+    response
 
-    # create new question or terminate if reached desired number of questions
+    # create new question or terminate if reached desired number of questions (`cont=False`)
     response, cont = continue_lesson(response, session_id)
-    if cont:
-        hermes.publish_continue_session(intent_message.session_id, response, INTENT_FILTER_GET_ANSWER)
-    else:
-        hermes.publish_end_session(session_id, response)
+
+    # different hermes publish based on value of `cont`
 
 
 def user_quits(hermes, intent_message):
@@ -151,10 +161,5 @@ def user_quits(hermes, intent_message):
     hermes.publish_end_session(session_id, response)
 
 
-with Hermes(MQTT_ADDR) as h:
-
-    h.subscribe_intent(INTENT_START, user_request_quiz) \
-        .subscribe_intent(INTENT_STOP, user_quits) \
-        .subscribe_intent(INTENT_DOES_NOT_KNOW, user_does_not_know) \
-        .subscribe_intent(INTENT_ANSWER, user_gives_answer) \
-        .start()
+if __name__ == "__main__":
+    main()
